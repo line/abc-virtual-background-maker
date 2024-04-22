3abfd62e2ee7ce09bc197d98d7d0b46250bbbb18
goun.choe <goun.choe@linecorp.com>
2024-04-22 10:52:45 +0900

    feat: update locale constants

--- src/constants/locale.ts
+++ src/constants/locale.ts
@@ -1,9 +1,11 @@
 import translationEn from "@/locales/en.json";
+import translationJa from "@/locales/ja.json";
 import translationKo from "@/locales/ko.json";
 
 export const Locale = {
   en: "en",
   ko: "ko",
+  ja: "ja",
 } as const;
 
 export const Locales = Object.keys(Locale);
@@ -11,4 +13,5 @@ export const Locales = Object.keys(Locale);
 export const LocaleResources = {
   [Locale["en"]]: { translation: translationEn },
   [Locale["ko"]]: { translation: translationKo },
+  [Locale["ja"]]: { translation: translationJa },
 };
